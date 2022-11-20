e3f984c702e79adefe2b3ca74df30fdf8a3527a6
matheus <matheusfavaretto27@gmail.com>
2022-11-20 10:54:12 -0300

    adicionado classe Registration e migrate ao banco de dados.

--- school/admin.py
+++ school/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from school.models import Student, Course
+from school.models import Student, Course, Registration
 
 class Students(admin.ModelAdmin):
     list_display = ('id', 'name', 'rg', 'cpf', 'birth_date',)
@@ -17,3 +17,9 @@ class Courses(admin.ModelAdmin):
     search_fields = ('Code_course',)
 
 admin.site.register(Course, Courses)
+
+class Registrations(admin.ModelAdmin):
+    list_display = ('id', 'student', 'course', 'period')
+    list_display_links = ('id',)
+
+admin.site.register(Registration, Registrations)
--- school/models.py
+++ school/models.py
@@ -23,3 +23,13 @@ class Course(models.Model):
     def __str__(self):
         return self.description
 
+class Registration(models.Model):
+        PERIOD = (
+        ('M', 'Morning'),
+        ('A', 'Afternoon'),
+        ('N', 'Nightly')
+        )
+        student = models.ForeignKey(Student, on_delete=models.CASCADE)
+        course = models.ForeignKey(Course, on_delete=models.CASCADE)
+        period = models.CharField(max_length=1, choices=PERIOD, blank=False, null=False, default='A')
+
