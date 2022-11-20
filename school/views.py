94cc61ba80e0bc1a01234fad8ccff4e3d3ae831b
matheus <matheusfavaretto27@gmail.com>
2022-11-20 09:41:37 -0300

    views.py modificado para viewset.

--- school/views.py
+++ school/views.py
@@ -1,6 +1,16 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from school.models import Student, Course
+from serializer import StudentSerializer, CourseSerializer
 
-def students(request):
-    if request.method == 'GET':
-        student = {'id':1, 'name':'Guilherme'}
-        return JsonResponse(student)
+
+class StudentsViewSet(viewsets.ModelViewSet):
+    """Showing all students"""
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CoursesViewSet(viewsets.ModelViewSet):
+    """Showing all courses"""
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    
